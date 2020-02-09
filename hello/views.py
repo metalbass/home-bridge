a21f275072431920a8e7b86f4e7ac137969cf8fa
metalbass_92 <xavier1923@gmail.com>
2020-02-09 12:44:35 +0100

    Implement token method

--- hello/views.py
+++ hello/views.py
@@ -1,62 +1,73 @@
 import urllib.parse
 
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.http import HttpResponse
-
+import mimetypes
+from django import http, shortcuts
 from .models import Greeting
 
 
-def query_dict_from_request(request, debug = False):
-  query = urllib.parse.urlparse(request).query
-  request_query_dict = (dict(urllib.parse.parse_qsl(query)))
+def get_request_parameters(request: http.HttpRequest, debug: bool = False):
+    query = urllib.parse.urlparse(str(request)).query
+    request_query_dict = dict(urllib.parse.parse_qsl(query))
 
-  if debug:
-    for key in request_query_dict:
-      print(key + ': ' + request_query_dict[key])
+    if debug:
+        for key in request_query_dict:
+            print(key + ': ' + request_query_dict[key])
 
-  return request_query_dict
+    return request_query_dict
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return shortcuts.render(request, "index.html")
 
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
 
     greetings = Greeting.objects.all()
 
-    return render(request, "db.html", {"greetings": greetings})
+    return shortcuts.render(request, "db.html", {"greetings": greetings})
 
 
 def api(request):
-    return HttpResponse("{}")
+    return http.HttpResponse("{}")
+
 
 def auth(request):
-    request_query_dict = query_dict_from_request(str(request))
-
+    # TODO: ensure it's a GET request
     # TODO: Ask for password/login or something before giving auth_code!
 
-    redirect_uri = request_query_dict['redirect_uri']
-    auth_code = 'AUTH_CODE_HERE'
-    state = request_query_dict['state']
+    request_parameters = get_request_parameters(request)
 
-    params = urllib.parse.urlencode(
-      {
-        'code': auth_code,
-        'state': state
-      }
+    redirect_uri = request_parameters['redirect_uri']
+    auth_code = 'AUTH_CODE_HERE'
+    state = request_parameters['state']
+
+    response_parameters = urllib.parse.urlencode(
+        {
+            'code': auth_code,
+            'state': state
+        }
     )
 
-    result = redirect_uri + '?' + params
-
-    return redirect(result)
+    return http.HttpResponseRedirect(redirect_uri + '?' + response_parameters)
 
 
 def token(request):
-    return HttpResponse("{}")
+    # TODO: make sure it's a POST request
 
+    json_res = {
+        "token_type": "bearer",
+        "access_token": "123access",
+        "expires_in": 24 * 60 * 60,  # 24h in seconds
+    }
+
+    request_parameters = get_request_parameters(request)
+    grant_type = request_parameters['grant_type']
+
+    if grant_type == "authorization_code":
+        json_res["refresh_token"] = "123refresh"
+
+    return http.HttpResponse(str(json_res), content_type="application/json")
