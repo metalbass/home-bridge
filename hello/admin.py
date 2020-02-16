d605add4ec572a1134ddab7ca6f06821ec9e2f4c
metalbass_92 <xavier1923@gmail.com>
2020-02-16 20:03:47 +0100

    Reminder to not delete the sneaky import *

--- hello/admin.py
+++ hello/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.apps import apps
-from .models import *
+
+from .models import *  # Import all models so they can be added to the admin panel
 
 admin.site.register(apps.get_app_config('hello').get_models())
