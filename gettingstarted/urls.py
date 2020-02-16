68fcdb17fa6f6919e12fd6ba284e832c62537c76
metalbass_92 <xavier1923@gmail.com>
2020-02-16 10:52:09 +0100

    Fix PEP8 imports need to be on top of the file

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-
-admin.autodiscover()
-
 import hello.views
 
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+admin.autodiscover()
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
