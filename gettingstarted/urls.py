430d7108cf518c9a427874c1b64d68e71deb3a60
metalbass_92 <xavier1923@gmail.com>
2020-02-29 09:14:53 +0100

    No url path redirects to admin

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 import hello.views
 
 admin.autodiscover()
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='admin/')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("", hello.views.index, name="index"),
+    path("admin/", admin.site.urls),
     path("api/", hello.views.api),
     path("auth/", hello.views.auth),
     path("token/", hello.views.token),
-    path("admin/", admin.site.urls),
 ]
